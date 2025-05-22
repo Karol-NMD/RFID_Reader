de2dbabcb8cd717cec4861d66d2be483c5f9ba87
Meta-Witness <metawitness@nanosatellitemissions.com>
2025-05-22 11:25:19 +0100

    RFIDReader2 declaring only antennae tx_power

--- RFIDReader2.py
+++ RFIDReader2.py
@@ -161,7 +161,7 @@ def main():
     config.start_inventory = False
     # config.tx_power = {0: 0, 1: 0}
     config.tx_power_dbm = 30
-    config.antennas = [0, 1]
+    config.antennas = [1, 2]
     config.report_every_n_tags = 1  # Report after every tag seen
     config.reader_mode = None  # or a valid string like 'AutoSetDenseReader'
     config.search_mode = None  # or a mode like 'DualTarget'
