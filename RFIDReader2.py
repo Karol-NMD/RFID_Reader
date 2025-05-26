feffe2d26e49c7856da564b6712a9280a50e1a87
Meta-Witness <metawitness@nanosatellitemissions.com>
2025-05-26 13:08:04 +0100

    Database added with SQLite (Worked by Dilan)

--- RFIDReader2.py
+++ RFIDReader2.py
@@ -200,9 +200,8 @@ def main():
     config = LLRPReaderConfig()
     config.reset_on_connect = True
     config.start_inventory = False
-    # config.tx_power = {0: 0, 1: 0}
-    config.antennas = [1]
-    config.tx_power_dbm = {1: 27.0}
+    config.tx_power = {1: 0, 2: 0}
+    config.antennas = [1, 2]
     config.report_every_n_tags = 1  # Report after every tag seen
     config.reader_mode = None  # or a valid string like 'AutoSetDenseReader'
     config.search_mode = None  # or a mode like 'DualTarget'
