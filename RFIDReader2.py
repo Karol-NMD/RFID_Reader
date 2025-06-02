a1eeea471997a946f88e2bb042bcec9b666eb367
Meta-Witness <metawitness@nanosatellitemissions.com>
2025-06-02 17:10:56 +0100

    Reader Mode Configuration + EPC Hex/Ascii Debugging(Test)

--- RFIDReader2.py
+++ RFIDReader2.py
@@ -236,7 +236,7 @@ def main():
     config.tx_power = {1: 0, 2: 0}
     config.antennas = [1, 2]
     config.report_every_n_tags = 1  # Report after every tag seen
-    config.reader_mode = 'AutoSetDenseReader'  # or a valid string like 'AutoSetDenseReader'
+    config.reader_mode = 'MaxThroughput'  # or a valid string like 'AutoSetDenseReader'
     config.search_mode = 'DualTarget'  # or a mode like 'DualTarget'
 
     # Configure the fields to include in each tag report
